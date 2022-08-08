6fa441d1a74cfb7c16ea0624742f40e90c717d97
KazukiJackal <110801777+KazukiJackal@users.noreply.github.com>
2022-08-08 03:51:34 -0500

    Update Main.java

--- Arreglos2Matrices/src/Main.java
+++ Arreglos2Matrices/src/Main.java
@@ -1,23 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        String cambios;
+        int i;
+        //Aquí se harán algunas modificaciones
+        
         String nombres[][] = {
                 {"Jose", "Maria", "Raul"},
                 {"Jean", "Vicente","Lucía"},
                 {"Junior", "Saul", "Laura"}
     };
-        //Saul
-       System.out.println(nombres[2][1]);
-     for(int j = 0; j<nombres[2].length; j++)
-    System.out.println(nombres[1][j]);
+       // Saul
+        //System.out.println(nombres[2][1]);*/
+    //  for(int j = 0; j<nombres[2].length; j++)
+       //     System.out.println(nombres[1][j]);*/
         for(int i = 0; i < nombres.length; i++){
             for(int j = 0; j<nombres[i].length; j++){
                 System.out.println(nombres[i][j]);
             }
-        //    System.out.println(nombres.indexOf(
+            System.out.println(nombres.indexOf(
 
-
+            )
         };
-}
 }
