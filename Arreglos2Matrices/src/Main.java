6c6d0fc3f77f6acee07ac34c00f5057aabc8c62c
KazukiJackal <nightraven_1023@hotmail.com>
2022-08-08 12:32:42 -0500

    Cambiodesdepc

--- Arreglos2Matrices/src/Main.java
+++ Arreglos2Matrices/src/Main.java
@@ -1,23 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x;
-        String Cambios;
-        String DesdePC;
-        //Aquí se harán algunas modificaciones
-        
+        int x = 0;
+        String Modificaciones = "";
+        int CantidadNombres;
+
         String nombres[][] = {
                 {"Jose", "Maria", "Raul"},
                 {"Jean", "Vicente","Lucía"},
                 {"Junior", "Saul", "Laura"}
     };
-       // Saul
-        //System.out.println(nombres[2][1]);*/
-    //  for(int j = 0; j<nombres[2].length; j++)
-       //     System.out.println(nombres[1][j]);*/
+        System.out.println(nombres[2][1]);
+            for(int j = 0; j<nombres[2].length; j++)
+        System.out.println(nombres[1][j]);
         for(int i = 0; i < nombres.length; i++){
             for(int j = 0; j<nombres[i].length; j++){
                 System.out.println(nombres[i][j]);
             }
         }
+        System.out.println("Modificaciones hechas desde la PC");
 }}
