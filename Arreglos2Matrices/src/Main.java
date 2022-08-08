7fc585e132db0f3705258531602ffe4ce019ad0b
KazukiJackal <110801777+KazukiJackal@users.noreply.github.com>
2022-08-08 02:26:37 -0500

    Add files via upload

---
+++ Arreglos2Matrices/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String nombres[][] = {
+                {"Jose", "Maria", "Raul"},
+                {"Jean", "Vicente","Lucía"},
+                {"Junior", "Saul", "Laura"}
+    };
+        /*Saul
+        System.out.println(nombres[2][1]);*/
+      /*  for(int j = 0; j<nombres[2].length; j++)
+            System.out.println(nombres[1][j]);*/
+        for(int i = 0; i < nombres.length; i++){
+            for(int j = 0; j<nombres[i].length; j++){
+                System.out.println(nombres[i][j]);
+            }
+            System.out.println(nombres.indexOf(
+
+            )
+        };
+}
+}
