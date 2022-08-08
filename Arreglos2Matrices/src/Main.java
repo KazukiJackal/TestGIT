aa9d6e0cc7f3cbabd66e4b91fe172e161ab2ff9c
KazukiJackal <110801777+KazukiJackal@users.noreply.github.com>
2022-08-08 12:28:18 -0500

    Update Main.java
    Cambio desde la web v2.0

--- Arreglos2Matrices/src/Main.java
+++ Arreglos2Matrices/src/Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        int i;
-        //Aquí se harán algunas modificaciones
-        
+    //Aquí se harán algunas modificaciones
+        int i = 0;
+        String cambios = "Nuevos Cambios";        
         String nombres[][] = {
                 {"Jose", "Maria", "Raul"},
                 {"Jean", "Vicente","Lucía"},
                 {"Junior", "Saul", "Laura"}
+        System.out.println(cambios);
     };
        // Saul
         //System.out.println(nombres[2][1]);*/
@@ -17,8 +18,5 @@ public class Main {
             for(int j = 0; j<nombres[i].length; j++){
                 System.out.println(nombres[i][j]);
             }
-            System.out.println(nombres.indexOf(
-
-            )
-        };
+        }
 }
